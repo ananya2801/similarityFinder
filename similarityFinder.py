281bee0e39d5f7559c07c4086bb743faf083cd3a
Ananya <ananyarao2807@gmail.com>
2021-05-31 17:32:26 +0400

    able to write to excel sheet

--- similarityFinder.py
+++ similarityFinder.py
@@ -9,19 +9,22 @@ xy = coordinate_from_string('AM4') # returns ('A',4)
 col = column_index_from_string(xy[0])
 xy = coordinate_from_string('AF4') 
 out_col = column_index_from_string(xy[0]) 
-print(col)
+print(out_col)
 input_sheet = wb['CTB 20']
 max_input_row = input_sheet.max_row
+#output_sheet['AF4'] = 'BLAH BLAH'
+output_sheet.cell(row=4, column = 32).value = "=\'CTB 20\'!AM13"
 
-
+  
 for i in range(1,max_output_row+1):
     cell_out = output_sheet.cell(row = i, column = 2)
     for j in range(1,max_input_row+1):
         cell = input_sheet.cell(row = j, column = 2)
         if cell.value != None:
             if cell_out.value == cell.value:
-                newcell = output_sheet.cell(row = i, column= out_col)
-                newcell = '=\'CTB 20\'!AM{}'.format(j) #input_sheet.cell(row = j, column = col).value)
-                wb.save("outputfile.xlsx")
+                output_sheet.cell(row=i,column=out_col).value = '=\'CTB 20\'!AM{}'.format(j)
+    #wb.save("outputfile.xlsx")  
+wb.save("outputfile.xlsx")   
+           
 
         
