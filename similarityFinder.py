22b2c7f27819d7868935c36d10e5fbaec8831bb6
Ananya <ananyarao2807@gmail.com>
2021-05-31 04:02:49 +0400

    when descrp is the same

--- similarityFinder.py
+++ similarityFinder.py
@@ -1,5 +1,27 @@
-import numpy as np 
-import pandas as pd 
-data = pd.read_csv("exampleSheet.csv")
-print(data)
-print(data["MONTH"])
+from openpyxl import load_workbook
+from openpyxl.utils.cell import coordinate_from_string, column_index_from_string
+wb = load_workbook(filename='Holiday_Inn_Al_Barsha_2020_old.xlsx')
+# output sheet
+output_sheet = wb['MMTB']
+max_output_row = output_sheet.max_row
+# getting column number from name : https://stackoverflow.com/a/12902801
+xy = coordinate_from_string('AM4') # returns ('A',4)
+col = column_index_from_string(xy[0])
+xy = coordinate_from_string('AF4') 
+out_col = column_index_from_string(xy[0]) 
+print(col)
+input_sheet = wb['CTB 20']
+max_input_row = input_sheet.max_row
+
+
+for i in range(1,max_output_row+1):
+    cell_out = output_sheet.cell(row = i, column = 2)
+    for j in range(1,max_input_row+1):
+        cell = input_sheet.cell(row = j, column = 2)
+        if cell.value != None:
+            if cell_out.value == cell.value:
+                newcell = output_sheet.cell(row = i, column= out_col)
+                newcell = '=\'CTB 20\'!AM{}'.format(j) #input_sheet.cell(row = j, column = col).value)
+                wb.save("outputfile.xlsx")
+
+        
